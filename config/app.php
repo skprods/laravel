c3053422263b810d20db29fe9ac964979547d646
Pavel Sklyar <ozpavel@yandex.ru>
2022-12-04 00:08:08 +0300

    Удалил лишние файлы

--- config/app.php
+++ config/app.php
@@ -186,15 +186,7 @@ return [
          * Package Service Providers...
          */
 
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
